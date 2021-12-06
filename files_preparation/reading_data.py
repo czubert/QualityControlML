b4562bdd545c1031e3d46b3842f10a776e680369
Pawel <czubercik@gmail.com>
2021-12-06 07:09:44 +0100

    Initial commit first steps are working grouped data is achieved

---
+++ files_preparation/reading_data.py
@@ -0,0 +1,119 @@
+import os
+import re
+import pandas as pd
+import numpy as np
+import utils
+
+# Output paths
+dir_path = 'data_output/step_1_reading_data'
+file_name = 'read_files'
+
+
+def main(file_names):
+    """
+    Returns|saves dict of tuples(metadata, data) consisting of DataFrames of single spectra
+    segregated by types of spectra (ag, au, ag_gb, au_bg
+        Additionally it gives corresponding file name as a feature name.
+    :param file_paths_of_one_type: list
+    :return: dict of list of tuples
+    """
+    files = {}
+    for file_names_types in file_names:
+        data = []
+        
+        for name in file_names[file_names_types]:
+            # Splits path name into two elements in a tuple. One is path and the second is name of a file
+            
+            if isinstance(name, str):
+                path_split = os.path.split(name)
+                
+                prefix = re.search(r'20.{8}',path_split[0]).group(0)
+                main_name = re.search(r'.*',path_split[1]).group(0)[0:-4]
+                
+                tmp_file_name = prefix + ' ' + main_name
+            else:
+                continue
+            
+            # adding name of file into metadata DataFrame
+            meta_df = read_metadata(name)
+            meta_df.loc['file_name'] = name
+            
+            data_df = read_spectrum(name)
+            
+            # Solution for files in which there were no values in th ecolumn "Raman Shift"
+            if data_df is None:
+                continue
+            
+            if len(main_name) > 10:
+                data_df.loc['id'] = tmp_file_name[0:7] + "_" + tmp_file_name[-1:]
+                data_df.rename(columns={'Dark Subtracted #1': main_name}, inplace=True)
+            else:
+                data_df.loc['id'] = tmp_file_name
+                data_df.rename(columns={'Dark Subtracted #1': tmp_file_name}, inplace=True)
+    
+    
+            # data_df.rename(columns={'Dark Subtracted #1': main_name}, inplace=True)
+            
+            
+            # dropping rows with NaN so after grouping there are less problems with NaNs
+            # data_df.dropna(how="any",inplace=True, axis=0)
+            
+            # creates list of tuples containing 2 elements metadata and data
+            data.append((meta_df, data_df))
+        
+        files[file_names_types] = data
+    
+    utils.save_as_joblib(files, file_name, dir_path)
+    return files
+
+
+def read_spectrum(filepath):
+    """
+    Reads numeric data from file and returns DataFrame
+    :param filepath: String
+    :return: DataFrame
+    """
+    read_params = {'sep': ';', 'skiprows': lambda x: x < 79 or x > 1500, 'decimal': ',',
+                   'usecols': ['Raman Shift', 'Dark Subtracted #1'],
+                   'skipinitialspace': True, 'encoding': "utf-8", 'na_filter': True}
+    
+    data_df = pd.read_csv(filepath, **read_params)
+    
+    data_df.dropna(axis=0, how="any")
+    
+    data_df = data_df[data_df.iloc[:, 0] > 253]
+    
+    # TODO sprawdzić, czy to ma wpływ na wyniki
+    data_df = data_df.astype({'Raman Shift': 'int'})
+    
+    # Solution for files in which there were no values in th ecolumn "Raman Shift"
+    if data_df.empty:
+        return None
+    
+    data_df.set_index('Raman Shift', inplace=True)
+    return data_df
+
+
+def read_metadata(filepath):
+    """
+    Reads metadata from file and returns DataFrame
+    :param filepath: String
+    :return: data frame
+    """
+    
+    read_params = {'sep': ';', 'skiprows': lambda x: x > 78, 'decimal': ',', 'index_col': 0,
+                   'skipinitialspace': True, 'encoding': "utf-8", 'header': None}
+    meta_df = pd.read_csv(filepath, **read_params)
+    
+    return meta_df
+
+
+def read_data(file_names, read_from_file=False):
+    if read_from_file:
+        if not os.path.isfile(dir_path + '//' + file_name + '.joblib'):
+            read_files = main(file_names)
+        else:
+            read_files = utils.read_joblib(file_name, dir_path)
+        return read_files
+    else:
+        return main(file_names)
---
+++ get_model.py
@@ -0,0 +1,68 @@
+"""
+Application responsible for training estimators based on data available
+"""
+import time
+from datetime import datetime
+from files_preparation import getting_names, reading_data, grouping_data
+
+now = datetime.now()
+
+print(f'Starting time: {now.strftime("%Y_%m_%d %H:%M:%S")}')
+print()
+
+
+# Loading data, separating it by ag/au/tlo ag/tlo au and separating it to metadata and data
+start_time = time.time()
+
+"""
+********************************************************************************
+GETTING FILE NAMES FOR FURTHER USE
+--------------------------------------------------------------------------------
+********************************************************************************
+"""
+
+print('Getting filenames...')
+
+file_names = getting_names.get_names(read_from_file=False)
+
+print(f'Data loaded in {round(time.time() - start_time, 2)} seconds')
+print()
+
+"""
+********************************************************************************
+Reading files (based on files_names) into DataFrames
+--------------------------------------------------------------------------------
+stored as Dictionary, where
+keys are ag, au, ag_bg, au_bg
+values are tuples(metadata, data)
+********************************************************************************
+"""
+
+start_time = time.time()
+print('Reading files into DataFrames...')
+
+# Getting files as a dict of tuples with metadata and data, keys are types of spectra (ag, au, ag_bg, au_bg
+read_files = reading_data.read_data(file_names, read_from_file=False)
+
+
+print(f'Data loaded in {round(time.time() - start_time, 2)} seconds')
+print()
+
+
+"""
+********************************************************************************
+Grouping Data
+--------------------------------------------------------------------------------
+Creating a DataFrame concatenated from all single spectra DataFrames
+from each type of spectra (ag, au, ag_bg, au_bg)
+One DataFrame per one Spectra Type
+********************************************************************************
+"""
+
+start_time = time.time()
+print('Grouping data...')
+grouped_files = grouping_data.group_data(read_files, read_from_file=False)
+
+
+print(f'Data loaded in {round(time.time() - start_time, 2)} seconds')
+print()
